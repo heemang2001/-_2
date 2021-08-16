c7666838ff3d713fe2ab03c9bb432b51f44379cc
heemang2001 <babypooh2001@naver.com>
2021-08-16 15:45:46 +0900

    암호 만들기

--- 완전탐색/암호 만들기.cpp
+++ 완전탐색/암호 만들기.cpp
@@ -8,10 +8,14 @@ using namespace std;
 int n, m;
 char arr[20];
 
+// 최소 1개이상의 모음 (a, e, i, o, u)
+// 최소 2개이상의 자음
+
 void DFS(int pos, string str, int ja, int mo)
 {
 	if (str.size() == n)
 	{
+		// 자음 2개이상 모음 1개 이상이 없으면 리턴
 		if (ja < 2 || mo < 1)
 		{
 			return;
@@ -22,11 +26,13 @@ void DFS(int pos, string str, int ja, int mo)
 
 	for (int i = pos; i < m; i++)
 	{
+		// 모음이면
 		if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u')
 		{
 			DFS(i + 1, str + arr[i], ja, mo + 1);
 		}
 			
+		// 자음이면
 		else
 		{
 			DFS(i + 1, str + arr[i], ja + 1, mo);
